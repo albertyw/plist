636fc1d02313924f3bd4b578e1283471c978ba2c
Albert Wang <albert@cellabus.com>
2016-02-17 01:25:14 -0800

    Add CodeClimate test reporter

--- test/test.rb
+++ test/test.rb
@@ -1,3 +1,8 @@
+begin
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+rescue LoadError
+end
 TEST_FILES = Dir.glob('test/test_*')
 TEST_FILES.each do |file|
   require File.basename(file)
